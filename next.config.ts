2a79dd84dcbca01a8517106ca44b7d6016d2dffc
fallow64 <fallow640@gmail.com>
2024-11-18 13:41:37 -0600

    fix: base path of next

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   output: "export", // a purely static build
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: "/osg-display/", // while being deployed on a test github pages
+  basePath: "/osg-display/", // while being deployed on a test github pages
 };
 
 export default nextConfig;
